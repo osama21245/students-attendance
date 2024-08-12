989a7ffb6b459251eb6c3edf738777b9e3920401
osama ahmed <osamagamil2070@gmail.com>
2024-08-12 08:50:11 +0300

    fix and push

--- lib/core/const/linksApi.dart
+++ lib/core/const/linksApi.dart
@@ -16,3 +16,21 @@ class Apilinks {
   static const String linkGetUserSessions =
       "https://lockapp.site/main_app/attendance/get_user_sessions.php";
 }
+
+class ApiErrors {
+  static const String badRequestError = "badRequestError";
+  static const String noContent = "noContent";
+  static const String forbiddenError = "forbiddenError";
+  static const String unauthorizedError = "unauthorizedError";
+  static const String notFoundError = "notFoundError";
+  static const String conflictError = "conflictError";
+  static const String internalServerError = "internalServerError";
+  static const String unknownError = "unknownError";
+  static const String timeoutError = "timeoutError";
+  static const String defaultError = "defaultError";
+  static const String cacheError = "cacheError";
+  static const String noInternetError = "noInternetError";
+  static const String loadingMessage = "loading_message";
+  static const String retryAgainMessage = "retry_again_message";
+  static const String ok = "Ok";
+}
